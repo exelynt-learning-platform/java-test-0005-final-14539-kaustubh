cd7e3fc8ae419333c2ce2f160129277e356ec83e
Kaustubh Sagale <kaustubhsagale007@gmail.com>
2026-03-14 12:08:44 +0530

    Added solution for JAVA TEST 0005

--- JAVATEST0005.java
+++ JAVATEST0005.java
@@ -2,12 +2,15 @@ public class JAVATEST0005 {
     public static void main(String[] args) {
 
         int num = 1;
+        int rows = 5;
 
-        for(int i = 1; i <= 5; i++) {          
-            for(int j = 1; j <= i; j++) {     
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
                 System.out.print(num + " ");
                 num++;
             }
+
             System.out.println();
         }
     }
